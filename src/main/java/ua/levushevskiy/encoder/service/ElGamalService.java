7922841b1c48528ec7e9b6744986959658fafc65
SLevushevskiy <S.Levushevsky@yandex.ru>
2018-12-27 19:33:13 +0200

    upd

--- src/main/java/ua/levushevskiy/encoder/service/ElGamalService.java
+++ src/main/java/ua/levushevskiy/encoder/service/ElGamalService.java
@@ -2,10 +2,13 @@ package ua.levushevskiy.encoder.service;
 
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
+import java.math.BigInteger;
+
 @Service
 public class ElGamalService {
 
-    Long p = 113L, g = 17L, x = 19L, k = 23L, y;
+    Long p = 23L, g = 5L, x = 7L, k = 5L, y;
 
 
     {
@@ -15,14 +18,29 @@ public class ElGamalService {
     public String encode(String message) {
         int hash = message.hashCode();
         long r = (long) Math.pow(g, k) % p;
-        long s = ((hash - x * r) * (long) Math.pow(k, -1)) % (p - 1);
+        long s = (hash - x * r) % (p - 1);
+        long res = 0;
+        for (int i = 0; ; i += p - 1)
+            if (s + i > 0 && (s + i) % k == 0) {
+                res = (s + i) / k;
+                break;
+            }
 
-        return hash + " " + r + " " + s;
+        return "r: " + r + "; s: " + res;
     }
 
     public Boolean decode(String message, long r, long s) {
         int hash = message.hashCode();
-        return (Math.pow(y, r) * Math.pow(r, s)) % p == Math.pow(g, hash) % p;
+        BigInteger num = BigInteger.valueOf(y);
+        BigInteger num2 = BigInteger.valueOf(r);
+        num2 = num2.pow((int) s);
+        num = num.pow((int) r);
+        num = num.multiply(num2);
+        num = num.remainder(BigInteger.valueOf(p));
+
+        BigInteger num3 = BigInteger.valueOf(g);
+        num3 = num3.modPow(BigInteger.valueOf(hash),BigInteger.valueOf(p));
+        return num.equals(num3);
     }
 
 }
